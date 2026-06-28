28c5bea0209853c907a2c5b8ce7647291629308e
Harshita2513 <harshitaupadhyay3152006@gmail.com>
2026-06-28 20:42:50 +0530

    Working

--- yummy_restaurant/accounts/models.py
+++ yummy_restaurant/accounts/models.py
@@ -1,10 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+# class Customer(models.Model) :
+#     name = models.CharField(max_length=100)
+#     email = models.EmailField()
+#     phoneno = models.CharField(max_length=15)
+#     date = models.DateField()
+#     time = models.TimeField()
+#     people = models.IntegerField()
+#     message = models.TextField()
 
-class Customer(models.Model) :
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    email = models.EmailField()
-    phoneno = models.IntegerField()
+    phoneno = models.CharField(max_length=15)
     date = models.DateField()
-    datetime = models.DateTimeField()
+    time = models.TimeField()
     people = models.IntegerField()
     message = models.TextField()
--- yummy_restaurant/accounts/views.py
+++ yummy_restaurant/accounts/views.py
@@ -6,59 +6,87 @@ from .models import Customer
 def register(request) :
     if request.method == "POST" :
         name = request.POST['name']
+        username = request.POST['username']
+        password = request.POST['password']
+        password2 = request.POST['password2']
         email = request.POST['email']
         phoneno = request.POST['phoneno']
         date = request.POST['date']
-        datetime = request.POST['datetime']
+        time = request.POST['time']
         people = request.POST['people']
         message = request.POST['message']
- 
-        if User.objects.filter(email = email).exists() :
-            messages.info(request, "User already exists")
+
+        if password == password2 :
+
+            if User.objects.filter(username = username).exists() :
+                messages.info(request, "User already exists")
+                return redirect("register")
+
+
+            elif User.objects.filter(email= email).exists() : 
+                messages.info(request, "Email already exists")
+                return redirect("register")
+
+            else : 
+                # user = User.objects.create_user(username=name, email=email)
+                # Customer.objects.create(
+                #     name = name,
+                #     email = email,
+                #     phoneno=phoneno, 
+                #     date= date, 
+                #     time = time, 
+                #     people=people, 
+                #     message= message
+                # )
+                # user.save()
+                user = User.objects.create_user(
+                username=username,
+                email=email,
+                password=password
+                )
+
+                Customer.objects.create(
+                    user = user,
+                    name=name,
+                    phoneno=phoneno,
+                    date=date,
+                    time=time,
+                    people=people,
+                    message=message
+                )
+                # user.save()
+
+                print("User saved successfully")
+                messages.success(request, "Customer booked successfully")
+
+                return render(request, "register.html", {'users' : user})
+        else:
+            messages.info(request, "Password didn't match")
             return redirect("register")
-
-
-        elif User.objects.filter(phoneno= phoneno).exists() : 
-            messages.info(request, "Phone no. already exists")
-            return redirect("/accounts/register")
-
-        else : 
-            user = User.objects.create_user(username=name, email=email)
-            Customer.objects.create(
-                phoneno=phoneno, 
-                date= date, 
-                datetime = datetime, 
-                people=people, 
-                message= message
-            )
-            user.save()
-            print("User saved successfully")
-            messages.success(request, "Customer booked successfully")
-
-            return redirect("register")
-
 
     else : 
          return render(request, "register.html")
 
-# def login(request) :
-#     if request.method == "POST" :
-#         username = request.POST['username']
-#         password = request.POST['password']
-#         user = auth.authenticate(username = username, password = password)
+def login(request) :
+    if request.method == "POST" :
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username = username, password = password)
+        # if Customer.objects.filter(username = username).exists() :
+        #     auth.login(request,)
 
-#         if user is not None : 
-#             auth.login(request, user)
-#             print("User Login successfully!!")
-#             return redirect('/')
-#         else : 
-#             messages.info(request, 'False Credentials')
-#             return redirect('login')
+        if user is not None : 
+            auth.login(request, user)
+            print("User Login successfully!!")
+            return redirect('/')
+        else : 
+            messages.info(request, 'False Credentials')
+            return redirect('login')
 
-#     else :
-#         # redirect('/')
-#         return render(request, 'login.html')
+    else :
+        # redirect('/')
+        return render(request, 'login.html')
 
-# def logout(request) :
-#     auth.logout(request)
-#     return  redirect('/')
+def logout(request) :
+    auth.logout(request)
+    return  redirect('/')
